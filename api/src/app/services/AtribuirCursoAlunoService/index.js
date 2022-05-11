605190f7ddbab4be51657f2793db77a2e25861d3
Kayque Pablo <kayquepablotzero@icloud.com>
2022-05-11 20:53:43 -0300

    UP

--- api/src/app/services/AtribuirCursoAlunoService/index.js
+++ api/src/app/services/AtribuirCursoAlunoService/index.js
@@ -1,10 +1,13 @@
-// import Aluno from '../../models/Aluno';
+import CursoAluno from '../../models/CursoAluno';
 
-// class AtribuirCursoAlunoService {
-//   async execute({ id_aluno, id_curso }) {
-//     // TODO
-//     return true;
-//   }
-// }
+class AtribuirCursoAlunoService {
+  async execute({ id_aluno, id_curso }) {
+    await CursoAluno.create({
+      id_pessoa: id_aluno,
+      id_curso
+    });
+    return true;
+  }
+}
 
-// export default new AtribuirCursoAlunoService();
+export default new AtribuirCursoAlunoService();
